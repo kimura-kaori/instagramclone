c64e791a085b6da2bbfd2ab62cbea81f22fe6d17
kaori kimura <tkhy_1125@icloud.com>
2022-04-19 16:00:47 +0900

    emailのカラムにインデックスを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validate :name, presence: true, length: { maximum: 30 }
   validate :email, presence: true, length: { maximum: 255 }
   format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
 end
