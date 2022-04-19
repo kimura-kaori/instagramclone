d402c6f4c0b9408b210c7daa57eb7dd179a25ae9
kaori kimura <tkhy_1125@icloud.com>
2022-04-19 15:56:36 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validate :name, presence: true, length: { maximum: 30 }
+  validate :email, presence: true, length: { maximum: 255 }
+  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
