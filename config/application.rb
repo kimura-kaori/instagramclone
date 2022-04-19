484e6efe21e99e963b4daa66e4f6fe1b2157ceee
kaori kimura <tkhy_1125@icloud.com>
2022-04-20 08:56:01 +0900

    pituresコントローラー作成、ルーティング設定

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,18 @@ Bundler.require(*Rails.groups)
 module Instagramclone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 5.1
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
