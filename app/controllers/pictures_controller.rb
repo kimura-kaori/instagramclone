a6ddabafabeceed29f46e2795ac3757f7e15569a
kaori kimura <tkhy_1125@icloud.com>
2022-04-20 10:17:13 +0900

    一覧画面を作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,14 +1,26 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
   end
 
   def new
     @picture = Picture.new
   end
 
+  def create
+    Picture.create(pictute_params)
+    redirect_to new_picture_path
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def pictute_params
+    params.require(:picture).permit(:image, :content)
   end
 end
