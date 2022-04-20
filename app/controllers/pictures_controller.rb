7760a5d83632c652353faed31958742dd277b580
kaori kimura <tkhy_1125@icloud.com>
2022-04-20 12:53:56 +0900

    画像アップロード機能作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,16 @@
 class PicturesController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update]
+
+  before_action :set_picture, only: [:show, :edit, :update, :destroy]
   def index
     @pictures = Picture.all
   end
 
   def new
-    @picture = Picture.new
+    if params[:back]
+      @picture = current_user.pictures.build(picture_params)
+    else
+      @picture = current_user.pictures.build
+    end
   end
 
   def create
@@ -25,23 +30,32 @@ class PicturesController < ApplicationController
   def edit
   end
 
+  def update
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "編集しました！"
+    else
+      render :edit
+    end
+  end
+
   def confirm
-    @picture = Picture.new(picture_params)
-    render :new if @picture.invalid?
+      @picture = current_user.pictures.build(picture_params)
+      render :new if @picture.invalid?
   end
 
   def destroy
-  @picture.destroy
-  redirect_to pictures_path, notice:"削除しました！"
+    @picture.destroy
+    redirect_to pictures_path, notice:"削除しました！"
   end
 
   private
 
-  def pictute_params
-    params.require(:picture).permit(:image, :content)
-  end
-
   def set_picture
     @picture = Picture.find(params[:id])
   end
+
+  def picture_params
+    params.require(:picture).permit(:image, :image_cache, :content)
+  end
+
 end
