7a9a7d3fa9bb88617225361daf6f1a615e6e510d
kaori kimura <tkhy_1125@icloud.com>
2022-04-21 13:06:19 +0900

    他人のプロフィールを閲覧できないように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if @user.id == current_user.id
+      render :show
+    else
+      redirect_to pictures_path
+    end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pictures#index'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   resources :pictures do
     collection do
